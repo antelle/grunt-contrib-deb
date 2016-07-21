38cde76c4c8e41a4d623a77d7b8df5d664beab7d
antelle <dmitryvit@gmail.com>
2016-07-21 22:11:13 +0300

    deps

--- gruntfile.js
+++ gruntfile.js
@@ -1,11 +1,16 @@
+'use strict';
+
+/*jshint node:true*/
+/*global grunt*/
+
 module.exports = function (grunt) {
-  grunt.initConfig({
-    jshint: {
-      all: ['lib/**/*.js', 'tasks/**/*.js']
-    }
-  });
-  
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  
-  grunt.registerTask('default', 'jshint');
+    grunt.initConfig({
+        jshint: {
+            all: ['lib/**/*.js', 'tasks/**/*.js']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.registerTask('default', 'jshint');
 };
--- tasks/deb.js
+++ tasks/deb.js
@@ -1,13 +1,18 @@
+'use strict';
+
+/*jshint node:true*/
+/*global grunt*/
+
 var deb = require('../lib/deb.js');
 
 module.exports = function (grunt) {
-  grunt.registerMultiTask('deb', 'package node apps into debian installers', function() {
-    var debPkg = deb(grunt);
-    
-    var done = this.async();
-    
-    debPkg.pack(this.options(), this.files, function (err) {
-      done(err);
-    });  
-  });
+    grunt.registerMultiTask('deb', 'package node apps into debian installers', function () {
+        var debPkg = deb(grunt);
+
+        var done = this.async();
+
+        debPkg.pack(this.options(), this.files, function (err) {
+            done(err);
+        });
+    });
 };
